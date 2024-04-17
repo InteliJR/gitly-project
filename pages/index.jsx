2f0a3918e751bf52d746a006c41a3419b192c45f
Rizzi26 <marco.meneguetti@sou.inteli.edu.br>
2024-04-17 16:07:16 -0300

    create next app V2.0

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TestComponent from '../components/TestComponent';
+
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Index Page</h1>
+            <TestComponent />
+        </div>
+    );
+};
+
+export default IndexPage;
