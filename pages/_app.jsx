14b43efc2473c0f128ab4057bcd01c92132756c6
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2024-04-17 17:40:57 -0300

    tmj

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import '../styles/globals.css'; 
-import Head from 'next/head';
+import "../styles/global.css";
+import React from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
